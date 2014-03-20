da0a24f6d7a4b2f242537b79e305ecfdebd4a376
C.C.T <changcheng.tu@gmail.com>
2014-03-20 16:05:26 +0800

    training system play audio

--- gui.py
+++ gui.py
@@ -1,28 +1,57 @@
-from Tkinter import Tk, Frame, BOTH
+from Tkinter import *
+from NeuroPy import NeuroPy
+import time
+from AppKit import NSSound
+sound = NSSound.alloc()
+
+class App:
+
+    #object1=NeuroPy("/dev/tty.MindWaveMobile-DevA",57600)
+
+    def __init__(self, master):
+
+        frame = Frame(master)
+        frame.pack()
+
+        self.button = Button(frame, text="QUIT", fg="red", command=frame.quit)
+        self.button.pack(side=LEFT)
+
+        self.hi_there = Button(frame, text="Understand", command=self.know)
+        self.hi_there.pack(side=LEFT)
+
+        self.hi_there = Button(frame, text="Don't understand", command=self.dont_know)
+        self.hi_there.pack(side=LEFT)
+
+        self.hi_there = Button(frame, text="Start", command=self.start_record)
+        self.hi_there.pack(side=LEFT)
+
+        #self.object1.start()
 
 
-class Example(Frame):
-  
-    def __init__(self, parent):
-        Frame.__init__(self, parent, background="black")   
-         
-        self.parent = parent
+    def know(self):
+        print 'I know'
+
+    def dont_know(self):
+        print 'dont know'
+
+    def start_record(self):
+        print sound.initWithContentsOfFile_byReference_('Cloud Nothings - Stay Useless.mp3', True)
+        sound.play()
+        for i in range(1,6):
+            #print self.object1.poorSignal
+            #print self.object1.delta
+            print 'record'
+            time.sleep(1)
         
-        self.initUI()
-    
-    def initUI(self):
-      
-        self.parent.title("Simple")
-        self.pack(fill=BOTH, expand=1)
-        
-
-def main():
-  
-    root = Tk()
-    root.geometry("600x450+300+300")
-    app = Example(root)
-    root.mainloop()  
+        sound.stop()
 
 
-if __name__ == '__main__':
-    main()  
+
+
+
+root = Tk()
+
+app = App(root)
+
+root.mainloop()
+root.destroy()
